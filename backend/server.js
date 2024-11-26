e4b8e08a2c80a7173aed60f300eb036182bac01b
Ely <elyverdugo88@gmail.com>
2024-11-26 15:13:10 -0300

    agregados

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors'); // Importa cors después de express
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const turnosRouter = require('./routes/turnos');
+const usuariosRoutes = require('./routes/usuarios'); 
 
 dotenv.config(); // Carga las variables de entorno al inicio
 
@@ -12,15 +13,18 @@ const app = express(); // Inicializa express
 app.use(cors()); // Habilita CORS para todas las solicitudes
 app.use(express.json()); // Middleware para parsear JSON
 
+// Usar las rutas de usuarios en /api/usuarios
+app.use('/api/usuarios', usuariosRoutes);
+
+// Usar las rutas de turnos en /api/turnos
+app.use('/api/turnos', turnosRouter);
+
 console.log("URI de MongoDB:", process.env.MONGODB_URI); // Confirmación de la URI
 
 // Conectar a MongoDB
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("Conectado a MongoDB"))
     .catch((error) => console.error("Error de conexión:", error));
-
-// Usar las rutas de turnos en /api/turnos
-app.use('/api/turnos', turnosRouter);
 
 app.get('/', (req, res) => {
     res.send('Servidor de reserva de turnos funcionando');
@@ -30,3 +34,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,33 +5,33 @@ import Header from './Componentes/Header/Header'
 import Seccion from './Componentes/Seccion/Seccion'
 import Canchas from './Componentes/Canchas/Canchas'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react'
 import Formulario from './Componentes/Formulario/Formulario'
 import Tarjetaquincho from './Componentes/Tarjeta/Tarjeta'
 import Eventos from './Componentes/Eventos/Eventos'
+import Registro from './Componentes/Registro/Registro'
+import Login from './Componentes/Login.jsx/Login'
 
 function App() {
-
   return (
     <Router>
-      <div className='contenedor-total'>
+      <div className="contenedor-total">
         <Header />
-        
-        <Routes basename="/entrega-final">
-        <Route path="/" element={<Seccion />} />
+        <Routes>
+          <Route path="/" element={<Seccion />} />
           <Route path="/seccion" element={<Seccion />} />
-           <Route path="/reservas" element={<div><Canchas /><Formulario /></div>} />
-          <Route path="/eventos" element={ <div> <Tarjetaquincho /><Eventos /></div>} />
-          
-         
-
+          <Route path="/reservas" element={<div><Canchas /><Formulario /></div>} />
+          <Route path="/eventos" element={<div><Tarjetaquincho /><Eventos /></div>} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-        
         <Footer />
-
-
       </div>
     </Router>
+
   )
 }
 
 export default App
+
+
